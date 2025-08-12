84f71bcf0425c1cb57f7abe29516d2e61773b113
stefany-newman <stef@stefanynewman.com>
2025-08-12 14:44:32 -0500

    JS - thank you for your message

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,17 @@ const mediaQuery = window.matchMedia('(min-width: 768px)')
 if (mediaQuery.matches) {
   navList.append(cta);
 }
+
+/* Thank you for your message. */
+
+const formHeader = document.querySelector("#form-header");
+const form = document.querySelector("form");
+const formInstructions = document.querySelector("#form-instructions");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevent the default form submission behavior
+    formHeader.textContent = "Thank you for your message. We will get back to you as soon as possible.";
+    form.style.display = "none";
+    formInstructions.style.display = "none";
+    formHeader.focus();
+});
