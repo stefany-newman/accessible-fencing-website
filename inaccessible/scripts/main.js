5ad21e51ae96dc0bbb3f1cbe0004327bdb82f9d2
stefany-newman <stef@stefanynewman.com>
2025-08-21 10:20:40 -0500

    Horizontal bar shows up on <200% zoom.

--- inaccessible/scripts/main.js
+++ inaccessible/scripts/main.js
@@ -1,9 +1,10 @@
-/* Thank you for your message. */
+/* Inaccessible scripts */
 
 const formHeader = document.querySelector("#contact-us-form-h");
 const form = document.querySelector("form");
 const formInstructions = document.querySelector("#form-instructions");
 
+/* Thank you for your message */
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // Prevent the default form submission behavior
     formHeader.textContent = "Thank you for your message. We will get back to you as soon as possible.";
@@ -11,3 +12,7 @@ form.addEventListener("submit", (e) => {
     formInstructions.style.display = "none";
     formHeader.focus();
 });
+
+/* Show scrollbar if zoomed in 400% or more, hence breaking 1.4.10 Reflow AA */
+window.addEventListener("resize", handleZoom);
+handleZoom(); // Initial check on page load
